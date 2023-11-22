c9267dcf53ec3dec4927ebbea002a622042db10c
do93boy <victorymutuku02@gmail.com>
2023-11-22 08:37:04 +0300

    first commit

---
+++ app/src/main/java/com/example/wazitoecommerce/ui/theme/screens/products/AddProductsScreen.kt
@@ -0,0 +1,43 @@
+package com.example.wazitoecommerce.ui.theme.screens.products
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
+import com.example.wazitoecommerce.ui.theme.WazitoECommerceTheme
+
+@Composable
+fun AddProductsScreen(navController:NavHostController){
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = "Add Products",
+            fontSize = 40.sp,
+            fontWeight = FontWeight.Bold,
+            fontFamily = FontFamily.Cursive
+        )
+
+    }
+}
+
+@Composable
+@Preview(showBackground = true)
+fun AddProductsScreenPreview(){
+    WazitoECommerceTheme {
+        AddProductsScreen(navController = rememberNavController())
+    }
+}
